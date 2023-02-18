fe2ad73712443317671afb1f7b4dd9702d0a400b
lioswin <lioswinrite@gmail.com>
2023-02-18 11:41:57 +0300

    init2

--- src/Details.jsx
+++ src/Details.jsx
@@ -4,7 +4,7 @@ import { useQuery } from "@tanstack/react-query";
 import fetchPet from "./fetchPet";
 import Carousel from "./Carousel";
 import ErrorBoundary from "./ErrorBoundary";
-import Modal from "./Modal";
+import Modal from "./modal";
 import AdoptedPetContext from "./AdoptedPetContext";
 
 const Details = () => {
